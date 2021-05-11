5d6e339ac3cfe7043dc2aab5ad489362c28a7a6e
Giorgio Garofalo <giogar2705@gmail.com>
2021-05-11 13:19:17 +0200

    Add logo to landing page

--- lib/constants/app_colors.dart
+++ lib/constants/app_colors.dart
@@ -8,4 +8,8 @@ class AppColors {
   static const TITLE_BAR_BUTTON = Colors.white;
   static const TITLE_BAR_CLOSE_OVER = Color(0xFFD32F2F);
   static const TITLE_BAR_CLOSE_PRESSED = Color(0xFFB71C1C);
+
+  static const BG_GRADIENT_1 = Color(0xFF318A94);
+  static const BG_GRADIENT_2 = Color(0xFF2E3F5A);
+  static const BG_GRADIENT_3 = Color(0xFF141C29);
 }
--- lib/ui/pages/landing_page.dart
+++ lib/ui/pages/landing_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:piktlab/constants/app_colors.dart';
 
 class LandingPage extends StatelessWidget {
   const LandingPage({Key key}) : super(key: key);
@@ -10,10 +12,25 @@ class LandingPage extends StatelessWidget {
       child: Container(
         decoration: BoxDecoration(
           gradient: LinearGradient(
-            begin: Alignment.bottomCenter,
-            end: Alignment.topCenter,
-            colors: [Colors.blue, Colors.purple]
-          )
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: [
+              AppColors.BG_GRADIENT_1,
+              AppColors.BG_GRADIENT_2,
+              AppColors.BG_GRADIENT_3,
+            ],
+            stops: [0, .6, 1],
+          ),
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            SvgPicture.asset(
+              "images/logo_light.svg",
+              width: MediaQuery.of(context).size.width / 3,
+            ),
+          ],
         ),
       ),
     );

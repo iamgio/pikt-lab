0baad2ce1e2f523e29d26ecdd4e6e7bdbcdb6551
Giorgio Garofalo <giogar2705@gmail.com>
2021-05-13 21:44:25 +0200

    Implement ColorScheme

---
+++ lib/pikt/ColorScheme.dart
@@ -0,0 +1,24 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:props/props.dart';
+
+/// Represents a Pikt .properties color scheme.
+class ColorScheme {
+
+  final Properties _properties;
+
+  ColorScheme.read(String path)
+    : _properties = Properties.loadString(File(path).readAsStringSync());
+
+  String operator [](String key) => _properties[key];
+
+  Color getColor(String key) => HexColor.fromHex(this[key]);
+  List<String> get keys => _properties.keys;
+  String getKeyByValue(value) => keys.firstWhere((element) => this[element] == value);
+}
+
+extension HexColor on Color {
+  /// Flutter [Color] from hexadecimal [hex].
+  static Color fromHex(String hex) => Color(int.parse('0xFF$hex'));
+}
